379ec6a02f6f7d40a30a0bf9e151841095ccc14f
Jessica Zemartis <jessica.a.zemartis@monmouth.edu>
2023-02-20 17:47:33 -0500

    Add search screen

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,32 +1,109 @@
 import './Card.css';
 import { cardBack } from './resources/rootIndex';
 import CardImages from './resources/cards/cardImageIndex';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 /** 
  * Styles
- * --full
- * --brief
- * --title
+ * --full: use for full page card descriptions
+ * --brief: used for quick shuffle
+ * --title: used for laying out lists
 */
 
-export default function Card({ cardProfile, flipped, style = 'brief' }) {
+export default function Card({ cardProfile, flipped, style = 'brief', close }) {
+    const [cardStyle, setCardStyle] = useState(style);
+
     let name = cardProfile.name;
     let imageName = cardProfile.img;
     let image = CardImages[imageName];
-    let keywords = cardProfile.keywords.join(", ");
+    let keywords = cardProfile.keywords;
+
+    let lightMeanings = cardProfile.meanings.light;
+    let shadowMeanings = cardProfile.meanings.shadow;
 
     if (flipped && style === 'brief') {
         return (
             <div className="Card">
                 <h1>{name}</h1>
                 <img src={image} />
-                <p><b>Keywords:</b> {keywords}</p>
+                <p className='briefKeywords'><b>Keywords:</b> {keywords.join(", ")}</p>
             </div>
         )
-    } else if (style === 'title')  {
+    } else if (style === 'title') {
         return (
             <div className="Card">
-                <img src={image} className='titleStyleImage'/>
+                <img src={image} className='titleStyleImage' />
+            </div>
+        )
+    } else if (style === 'full') {
+        return (
+            <div className='Card fullCard'>
+                <div className='left'>
+                    <div className='infoColumn'>
+                        <h1>{name}</h1>
+                        <img src={image} className='titleStyleImage' />
+                        {
+                            (cardProfile.archetype) ?
+                                <p className='archetype'><i>{cardProfile.archetype}</i></p> :
+                                <p className='archetype'><i>{cardProfile.affirmation}</i></p>
+                        }
+                    </div>
+                </div>
+                <div className='cardBody'>
+                        <div className='fullCardHeader'>
+                            <span className='headerKeywords'><h2>{keywords.join(' • ')}</h2></span>
+                            <div onClick={() => { close() }}><FontAwesomeIcon icon={faXmark} size={'xl'} /></div>
+                        </div>
+                        <div className='horizDivider'></div>
+                    <div className='infoColumn'>
+                        <div className='infoRow fullCard shrink'>
+                            <span className='fact'><h4>Number</h4><p>{cardProfile.number}</p></span>
+                            <span className='fact'><h4>Element</h4><p>{cardProfile.elemental}</p></span>
+                            <span className='fact'><h4>Astrology</h4><p>{cardProfile.astrology}</p></span>
+                        </div>
+                        <div className='infoColumn compact'>
+                            {
+                                (cardProfile.mythic)  ?
+                                <>
+                                <h4>Mythology</h4>
+                                <p className='fact'>{cardProfile.mythic}</p>
+                                </>
+                                :
+                                <>
+                                <h4>Numerology</h4>
+                                <p className='fact'>{cardProfile.numerology}</p>
+                                </>
+                            }
+                                
+                            </div>
+                        <div className='infoRow'>
+                            <div className='infoColumn'>
+                                <h4>Light Meanings</h4>
+                                <div className='meaningsContainer'>
+                                    {
+                                        lightMeanings.map((meaning, index) => (
+                                            <li>{meaning}</li>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                            <div className='infoColumn'>
+                                <h4>Shadow Meanings</h4>
+                                <div className='meaningsContainer'>
+                                    {
+                                        shadowMeanings.map((meaning, index) => (
+                                            <li>{meaning}</li>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
             </div>
         )
     } else {
@@ -38,5 +115,5 @@ export default function Card({ cardProfile, flipped, style = 'brief' }) {
             </div>
         )
     }
-    
+
 }
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './Search.css';
 import CardImages from './resources/cards/cardImageIndex';
 import Card from './Card';
@@ -8,44 +9,76 @@ import { majorArcana, wands, cups, swords, pentacles } from './resources/icons/i
 
 export default function Search() {
     const tarotSections = new Map();
-    tarotSections.set("majorArcana", TarotLibrary.slice(0, 22));
-    tarotSections.set("cups", TarotLibrary.slice(22, 36));
-    tarotSections.set("swords", TarotLibrary.slice(36, 50));
-    tarotSections.set("wands", TarotLibrary.slice(50, 64));
-    tarotSections.set("pentacles", TarotLibrary.slice(64, 78));
+    tarotSections.set("Major Arcana", TarotLibrary.slice(0, 22));
+    tarotSections.set("Cups", TarotLibrary.slice(22, 36));
+    tarotSections.set("Swords", TarotLibrary.slice(36, 50));
+    tarotSections.set("Wands", TarotLibrary.slice(50, 64));
+    tarotSections.set("Pentacles", TarotLibrary.slice(64, 78));
 
-    const [currentFilter, setCurrentFilter] = useState("majorArcana");
+    const [currentFilter, setCurrentFilter] = useState("Major Arcana");
     const [cardData, setCardData] = useState(tarotSections.get(currentFilter));
+    const [modalVisible, setModalVisible] = useState(false);
+    const [card, setCard] = useState({});
 
     function setFilter(filter) {
         setCurrentFilter(filter);
         setCardData(tarotSections.get(filter));
+        if (modalVisible) onCloseDetails();
     }
 
-    function CardContainer() {
+    const onOpenDetails = (card) => {
+        setCard(card);
+        setModalVisible(true);
+    };
 
-        return (
-            <div className='cardsContainer'>
-                {cardData.map((card, index) => (
-                    <Card cardProfile={card} flipped={true} style='title' key={index  + card.name}/>
-                ))}
-            </div>
-        )
+    const onCloseDetails = () => { setModalVisible(false); };
+
+    function CardContainer() {
+        if (modalVisible) {
+            return (
+                <div className='cardsContainer full'>
+                    <Card cardProfile={card}
+                        flipped={true}
+                        style='full'
+                        close={onCloseDetails}
+                    />
+                </div>
+            )
+        } else {
+            return (
+                <div className='cardsContainer'>
+                    {cardData.map((card, index) => (
+                        <div className='cardWrapper' onClick={() => { onOpenDetails(card) }}>
+                            <Card cardProfile={card}
+                                flipped={true}
+                                style='title'
+                                key={index + card.name}
+                            />
+                        </div>
+                    ))}
+                </div>
+            )
+        }
     }
 
     return (
         <>
             <div className='Search-body'>
                 <div className='iconNavigation'>
-                    <img src={majorArcana} className='icon' onClick={() => { setFilter("majorArcana") }} />
-                    <img src={wands} className='icon' onClick={() => { setFilter("wands") }} />
-                    <img src={cups} className='icon' onClick={() => { setFilter("cups") }} />
-                    <img src={swords} className='icon' onClick={() => { setFilter("swords") }} />
-                    <img src={pentacles} className='icon' onClick={() => { setFilter("pentacles") }} />
+                    <img src={majorArcana} className='icon' onClick={() => { setFilter("Major Arcana") }} />
+                    <img src={wands} className='icon' onClick={() => { setFilter("Wands") }} />
+                    <img src={cups} className='icon' onClick={() => { setFilter("Cups") }} />
+                    <img src={swords} className='icon' onClick={() => { setFilter("Swords") }} />
+                    <img src={pentacles} className='icon' onClick={() => { setFilter("Pentacles") }} />
                 </div>
                 <div className='vertDivider'></div>
                 <div className='cardsSearchContainer'>
-                    <SearchBar />
+                    {(!modalVisible)  ? <div className='searchHeader'>
+                        <h1 className='cardTitle'>
+                            {currentFilter}
+                        </h1>
+                        <SearchBar />
+                    </div> : ''}
                     <CardContainer />
                 </div>
             </div>
